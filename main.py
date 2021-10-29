32d888b1cb858b6e9518104c0d5c404cc375a9af
codefloww <kryvpaul@gmail.com>
2021-10-30 01:01:08 +0300

    added window player,human classes

--- main.py
+++ main.py
@@ -8,42 +8,82 @@ import stage3
 import stage4
 import pygame
 
+
 class Window:
-    def __init__(self,width,height,audio,background) -> None:
+    def __init__(self, width, height, audio, background) -> None:
         self.x = 0
         self.y = 0
         self.width = width
         self.height = height
-        self.audio = pygame.mixer.music.load(os.path.join("Assets",audio))
-        self.screen = pygame.display.set_mode((self.width,self.height))
-        self.background = pygame.image.load(os.path.join("Assets",background))
+        self.audio = pygame.mixer.music.load(os.path.join("Assets", audio))
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.background = pygame.image.load(os.path.join("Assets", background))
+        self.objects = []
 
     def get_size(self):
-        return (self.width,self.height)
+        return (self.width, self.height)
 
-    def update_screen(self,objects = []):
-        self.screen.blit(self.background,(self.x,self.y))
+    def update_screen(self, objects=[]):
+        self.screen.blit(self.background, (self.x, self.y))
         for object in objects:
-            self.screen.blit(object.get_image(),(object.x,object.y))
+            self.screen.blit(object.get_image(), (object.get_x(), object.get_y()))
+        pygame.display.update()
 
-    def move_background(self,vel):
-        self.x-=vel
+    def move_background(self, vel):
+        self.x -= vel
 
-    def play_audio(self):
-        pygame.mixer.music.play(-1)
+    def play_audio(self, action):
+        if action == "play":
+            pygame.mixer.music.unpause()
+        elif action == "stop":
+            pygame.mixer.music.pause()
+        elif action == "start":
+            pygame.mixer.music.play(-1)
+
+    def add_object(self, object):
+        self.objects.append(object)
 
 
 class Player:
+    def __init__(self, x, y, health, bowe, state) -> None:
+        self.health = health
+        self.bowe = bowe
+        self.state = state
+        self.x = x
+        self.y = y
+
+    def change_health(self, change):
+        self.health += change
+
+    def change_bowe(self, change):
+        self.bowe += change
+
+    def change_state(self):
+        self.state = "Human" if self.state == "Wolf" else "Wolf"
+
+
+class Human(Player):
     def __init__(self) -> None:
-        pass
+        self.velocity = 5
+        self.mass = 5
+        self.width = 100
+        self.height = 100
+        self.texture = pygame.transform.scale(
+            pygame.image.load(os.path.join("Assets", "space.png")),
+            (self.width, self.height))
+        self.rigid = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def get_image(self):
+        return self.texture
 
 
-class Human:
-    def __init__(self) -> None:
-        pass
-
-
-class Wolf:
+class Wolf(Player):
     def __init__(self) -> None:
         pass
 
@@ -73,14 +113,18 @@ class Buttons:
         pass
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     pygame.init()
-    menu = Window(1920,1080,'Beholder.mp3','space.png')
-    menu.play_audio()
+    menu = Window(1920, 1080, "Beholder.mp3", "space.png")
+    menu.play_audio("start")
     running = True
-    objects = []
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        menu.update_screen(objects)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    menu.play_audio("stop")
+                if event.key == pygame.K_p:
+                    menu.play_audio("play")
+        menu.update_screen()
