cd182303567cf89d02f771bf8b34e6f0d4df77f6
shnasta <shvets.nasta@gmail.com>
2021-10-31 13:34:34 +0200

    n

--- stage1.py
+++ stage1.py
@@ -7,6 +7,18 @@ pygame.mixer.init()
 pygame.font.init()
 
 
+def path(objects):
+    k=1
+    length = main.Road(k, 1000, "ground", 17000, 100, "ground.png")
+    objects.append(length)
+    k+=500
+
+    sail1 = main.Road(k, 900, "ground", 150, 100, "ground.png")
+    objects.append(sail1)
+    k+=300
+    sail2 = main.Road(k, 800, "ground", 150, 100, "ground.png")
+    objects.append(sail2)
+
 def stage():
     stage1 = main.Window(1920, 1080, "Oles.mp3", "background1.jpg")
     stage1.play_audio("start")
@@ -16,10 +28,7 @@ def stage():
     player = main.Wolf(100, 100, 100, 10, "Wolf")
     moveable.append(player)
     objects.append(player)
-    soil1 = main.Road(100, 500, "ground", 900, 50)
-    soil2 = main.Road(300, 400, "ground", 100, 300)
-    objects.append(soil1)
-    objects.append(soil2)
+    path(objects)
 
     clock = pygame.time.Clock()
     while running:
